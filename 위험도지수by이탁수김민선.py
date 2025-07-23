6a6d2185aa01a1273b76c13445b7106a19ac73da
push-line <rosieu98@naver.com>
2025-07-23 11:09:34 +0900

    Update 위험도지수by이탁수김민선.py

--- 위험도지수by이탁수김민선.py
+++ 위험도지수by이탁수김민선.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import numpy as np
 import pandas as pd
-from datetime import date
+import datetime
 import gspread
 from google.oauth2.service_account import Credentials
 
