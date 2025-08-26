a168a76905a9d34cc3899cb37fcd34d5c6ed657c
push-line <rosieu98@naver.com>
2025-08-27 02:02:30 +0900

    Update 위험도지수by이탁수김민선.py

--- 위험도지수by이탁수김민선.py
+++ 위험도지수by이탁수김민선.py
@@ -188,7 +188,7 @@ else:
         br, er, risk = calculate_risk(info, row["temp"], row["humidity"])
         level = interpret_index(risk)
         risk_list.append({
-            "요일": row["date}].strftime("%a"),
+            "요일": row["date"].strftime("%a"),
             "날짜": row["date"].strftime("%m-%d"),
             "평균 온도(°C)": round(row["temp"], 1),
             "평균 습도(%)": round(row["humidity"], 1),
@@ -198,5 +198,6 @@ else:
     st.dataframe(pd.DataFrame(risk_list).head(5))
 
 st.caption("※본 데이터는 기상청 및 OpenWeatherMap API 기반으로 수집되었습니다.")
+
 
 
