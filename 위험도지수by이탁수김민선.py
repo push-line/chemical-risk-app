85916804bc9ff8f195ea338c72d81d57e7bc328a
push-line <rosieu98@naver.com>
2025-09-26 03:54:23 +0900

    Update 위험도지수by이탁수김민선.py

--- 위험도지수by이탁수김민선.py
+++ 위험도지수by이탁수김민선.py
@@ -212,7 +212,7 @@ with col2:
             f"<div style='font-size:25px;font-weight:700;color:{color};"
             "background:#fff;padding:8px 12px;border-radius:12px;display:inline-block;'>"
             f"등급: {grade_label}</div>", unsafe_allow_html=True)
-        st.markdown(
+         st.markdown(
             f"<div style='font-size:28px;font-weight:800;color:{color};"
             "background:#fff;padding:8px 12px;border-radius:12px;display:inline-block;'>"
             f"위험지수: {risk_now}%</div>", unsafe_allow_html=True
@@ -252,6 +252,7 @@ else:
     st.dataframe(pd.DataFrame(risk_list).head(5))
 
 st.caption("※본 데이터는 기상청 및 OpenWeatherMap API 기반으로 수집되었습니다.") 
+
 
 
 
